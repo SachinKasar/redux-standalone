9641b667d16918252ce161a8ff11d50293de3117
Sachin C Kasar <cse.sck@gmail.com>
2018-01-18 11:45:51 +0530

    updated
    updated

--- src/app/index.js
+++ src/app/index.js
@@ -2,39 +2,55 @@ import {createStore} from "redux";
  
 const reducer = (state, action) => {
 	switch (action.type) {
-		case 'ADD':
-			state = state + action.payload;
+		case 'UPDATE_NAME':
+			state.name = action.payload;
 			break;
-		case 'SUBTRACT':
-			state = state - action.payload;
+		case 'ADD_PRICE':
+			state.price = Number(state.price) + Number(action.payload);
 			break;
+        case 'PRICE_DISCOUNT':
+            state.price = Number(state.price) - Number(action.payload);
+            break;
+        case 'QUERY_INITIAL_STATE':
+             break;
 	}
 	return state;
 };
 
-const store = createStore(reducer,1);
+const initialState = {name:'Two Wheeler Name', price:100000};
+const store = createStore(reducer,initialState);
 
 store.subscribe(() => {
 	console.log("Store state - ", store.getState());
 });
 
+
 store.dispatch({
-	type: "ADD",
-	payload:10
+	type: "QUERY_INITIAL_STATE"
 });
 
 store.dispatch({
-	type: "ADD",
-	payload:20
+	type: "UPDATE_NAME",
+	payload:"Splendor"
 });
 
 store.dispatch({
-	type: "ADD",
-	payload:30
+	type: "UPDATE_NAME",
+	payload:"Splendor+"
+});
+
+store.dispatch({
+	type: "ADD_PRICE",
+	payload:100
 });
 
 
 store.dispatch({
-	type: "SUBTRACT",
-	payload:30
+	type: "ADD_PRICE",
+	payload:200
+});
+
+store.dispatch({
+	type: "PRICE_DISCOUNT",
+	payload:5000
 });
